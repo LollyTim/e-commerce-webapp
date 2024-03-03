5ace4ed1713e70f65bd60727ccffbe303068593f
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-03-03 18:36:56 +0100

    updated

---
+++ app/api/register/route.ts
@@ -0,0 +1,18 @@
+import bcrypt from "bcrypt";
+import client from "@/libs/prismadb";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { name, email, password } = body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await client?.user.create({
+    data: {
+      name,
+      email,
+      hashedPassword,
+    },
+  });
+
+  return NextResponse.json(user);
+}
--- app/register/RegisterForem.tsx
+++ app/register/RegisterForem.tsx
@@ -7,8 +7,10 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form"
 import Button from "../components/Button"
 import Link from "next/link"
 import { AiOutlineGoogle } from "react-icons/ai"
-
-
+import axios from "axios"
+import toast from "react-hot-toast"
+import { signIn } from "next-auth/react"
+import { useRouter } from "next/navigation"
 
 const RegisterForem = () => {
 
@@ -20,12 +22,36 @@ const RegisterForem = () => {
             password: ""
         }
     })
+    const router = useRouter()
 
     const onSubmit: SubmitHandler<FieldValues> = (data) => {
-        {
-            setIsLoading(true)
-            console.log(data)
-        }
+        axios
+            .post("/api/register", data)
+            .then(() => {
+                toast.success("Account created successfully");
+                signIn("credentials", {
+                    email: data.email,
+                    password: data.password,
+                    redirect: false,
+                }).then((callback) => {
+                    if (callback?.ok) {
+                        router.push("/cart");
+                        router.refresh();
+                        toast.success("Logged In");
+                    }
+
+                    if (callback?.error) {
+                        toast.error(callback.error);
+                    }
+                });
+            })
+            .catch(() => {
+                toast.error("Something went wrong");
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+
     }
 
     return (
