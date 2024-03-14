710e62cb8f29030a55e51ec09fe78d11cc26b0d3
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-03-14 03:27:15 +0100

    doing

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -4,14 +4,16 @@ import ProductDetails from './ProductDetails';
 import Container from '@/app/components/Container';
 import ListRating from './ListRating';
 import { products } from '@/utils/products';
+import getProductById from '@/actions/getProductById';
+import NullData from '@/app/components/NullData';
 interface IParams {
     product: string
     productId?: string
 }
-const Product = ({ params }: { params: IParams }) => {
-    console.log("params: ", params);
+const Product = async ({ params }: { params: IParams }) => {
+    const product = await getProductById(params)
+    if (!product) return <NullData title='Oops! Product with the given ID does not exist' />
 
-    const product = products.find((item) => item.id === params.productId)
     return (
         < div className='p-8' >
             <Container>
